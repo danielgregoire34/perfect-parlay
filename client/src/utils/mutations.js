85ae57559da930ecfa1ff1e425026a08f06f72fb
Kenji Fleming <kwf1999@gmail.com>
2023-03-23 20:22:19 -0400

    added the utils folder for the src directory

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,62 @@
+//mutations.js
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookData: bookInput!) {
+        saveBook(bookData: $bookData) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+//queries.js
+import gql from 'graphql-tag';
+
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
