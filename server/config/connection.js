852f62c155d5f7611138404704d1ee3d1a6685e6
unknown <danielgregoire34@gmail.com>
2023-03-26 19:00:40 -0400

    changed connection localhost

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //mongodb://localhost/social-network-api
 mongoose.connect( 
-    process.env.MONGO_URI || 'mongodb://localhost/social-network-api',
+    process.env.MONGO_URI || 'mongodb://127.0.0.1/social-network-api',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
