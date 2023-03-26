22e540f4051416231f090676bd0d9f95b188f1b9
Kenji Fleming <kwf1999@gmail.com>
2023-03-25 21:00:31 -0400

    added mailto functionality to the contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -6,35 +6,40 @@ const Contact = () => {
                 <div className="Kenji contact-box">
                     Kenji Fleming
                     <div>
-                        <a href="kwf1999@gmail.com">Email</a>
+                        <a href="mailto:kwf1999@gmail.com">Email</a>
+                        <br></br>
                         <a href="https://github.com/N3tRunn3rr">GitHub</a>
                     </div>
                 </div>
                 <div className="Kennedy contact-box">
                     Kennedy Hayles
                     <div>
-                        <a href="kennedyhayles@gmail.com">Email</a>
+                        <a href="mailto:kennedyhayles@gmail.com">Email</a>
+                        <br></br>
                         <a href="https://github.com/kennedyisn">GitHub</a>
                     </div>
                 </div>
                 <div className="Daniel contact-box">
                     Daniel Gregoire
                     <div>
-                        <a href="danielgregoire34@gmail.com">Email</a>
+                        <a href="mailto:danielgregoire34@gmail.com">Email</a>
+                        <br></br>
                         <a href="https://github.com/danielgregoire34">GitHub</a>
                     </div>
                 </div>
                 <div className="Keenan contact-box">
                     Keenan Goode
                     <div>
-                        <a href="chasegoode7@gmail.com">Email</a>
+                        <a href="mailto:chasegoode7@gmail.com">Email</a>
+                        <br></br>
                         <a href="https://github.com/flayouth">GitHub</a>
                     </div>
                 </div>
                 <div className="Myles contact-box">
                     Myles Nichols
                     <div>
-                        <a href="mylesnichols21@yahoo.com">Email</a>
+                        <a href="mailto:mylesnichols21@yahoo.com">Email</a>
+                        <br></br>
                         <a href="https://github.com/MylesNichols">GitHub</a>
                     </div>
                 </div>
