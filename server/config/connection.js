46c4e7ab4532d17194d143f6cb7f84a6964b950d
unknown <danielgregoire34@gmail.com>
2023-03-26 21:32:10 -0400

    another fix to connectionjs

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //mongodb://localhost/perfect-parlay
 //mongodb://127.0.0.1:27017/perfect-parlay
 mongoose.connect( 
-    process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/perfect-parlay',
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/perfect-parlay',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
