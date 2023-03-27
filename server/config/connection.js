28d0de29f62b7ee6ab40df9f2e3089c3d5da0e96
unknown <danielgregoire34@gmail.com>
2023-03-26 20:18:51 -0400

    another heroku fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-//mongodb://localhost/social-network-api
+//mongodb://localhost/perfect-parlay
 //mongodb://127.0.0.1:27017/perfect-parlay
 mongoose.connect( 
-    process.env.MONGO_URI || 'mongodb://localhost/perfect-parlay',
+    process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/perfect-parlay',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
