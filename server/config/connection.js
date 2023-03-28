2f89fce0ecdcca1bae98b2d8e1210ddb0bdbba52
unknown <danielgregoire34@gmail.com>
2023-03-27 22:45:29 -0400

    deployement

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect( 
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/perfect-parlay',
+    process.env.MONGO_DB_URI || 'mongodb://127.0.0.1:27017/perfect-parlay',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
